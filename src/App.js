bbf6a353ffa475615e196b642f609741972300cb
Rhythm Bhiwani <47269183+rhythmbhiwani@users.noreply.github.com>
2023-07-28 06:08:20 +0530

    Changed HashRouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 import { useMediaQuery } from "react-responsive";
